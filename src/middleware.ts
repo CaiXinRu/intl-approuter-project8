f87fd8da17dbfde51c43a47dbcaf5e595b1d1dc2
cxr <claire_129as@yahoo.com.tw>
2024-01-17 23:51:53 +0800

    test: try to build getLocale but got failed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,38 +1,39 @@
-import createMiddleware from "next-intl/middleware";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { i18n } from "../i18n.config";
+import createIntlMiddleware from 'next-intl/middleware';
+import {NextRequest, NextResponse} from 'next/server';
+import {i18n} from '../i18n.config'
 import { match as matchLocale } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
-import { request } from "http";
-
+ 
 function getLocale(request: NextRequest): string | undefined {
-  // 將 request headers 轉換成 negotiator 需要的格式
   const negotiatorHeaders: Record<string, string> = {};
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
 
-  // 取得所有支援的語言
   // @ts-ignore locales are readonly
   const locales: string[] = i18n.locales;
-
-  // 使用 Negotiator 判斷 request 的優先語言
   const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
-
-  // 使用matchLocale會做陣列比對，取最多次出現且順序第一的語言
+  // matchLocale會做陣列比對，取最多次出現且順序第一的語言
   const locale = matchLocale(languages, locales, i18n.defaultLocale);
   return locale;
 }
-
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: i18n.locales,
-  // Used when no locale matches
-  defaultLocale: i18n.defaultLocale,
-  localePrefix: "always",
-  localeDetection: false,
-});
-
+export default async function middleware(request: NextRequest) {
+  // Step 1: Use the incoming request (example)
+  const locale = getLocale(request);
+  const defaultLocale =  locale  === 'zh' ? 'zh' : 'en'
+  // Step 2: Create and call the next-intl middleware (example)
+  const handleI18nRouting = createIntlMiddleware({
+    locales: i18n.locales,
+    defaultLocale: defaultLocale
+  });
+  const response = handleI18nRouting(request);
+ 
+  // Step 3: Alter the response (example)
+  response.headers.set('x-your-custom-locale', defaultLocale);
+ 
+   return response;
+  
+}
+ 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ["/", "/(zh|en)/:path*"],
+  matcher: ['/', '/(zh|en)/:path*']
 };
